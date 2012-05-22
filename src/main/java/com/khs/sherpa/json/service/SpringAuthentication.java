c533e5dbfcb85d2fea32d5c293ec677df3c5818c
Mark Determan <mdeterman@gmail.com>
2012-05-22 11:40:16 -0500

    update the spring auth

--- src/main/java/com/khs/sherpa/json/service/SpringAuthentication.java
+++ src/main/java/com/khs/sherpa/json/service/SpringAuthentication.java
@@ -1,9 +1,13 @@
 package com.khs.sherpa.json.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 
 import com.khs.sherpa.exception.SherpaPermissionExcpetion;
 
@@ -19,18 +23,21 @@ public class SpringAuthentication implements UserService {
 //		authenticationManager = this.ctx.getBean(AuthenticationManager.class);
 //	}
 	
-	public void authenticate(String username, String password) {
+	public String[] authenticate(String username, String password) {
 		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
 		
 		Authentication authentication = authenticationManager.authenticate(token);
 		if(authentication.isAuthenticated() == false) {
 			throw new SherpaPermissionExcpetion("username and/or password is incorrect");
 		}
-	}
-	
-
-	public void adminAuthenticate(String userid, String password) throws AuthenticationException {
-		// TODO Auto-generated method stub
+		
+		List<String> roles = new ArrayList<String>();
+		for(GrantedAuthority auth: authentication.getAuthorities()) {
+			roles.add(auth.getAuthority());
+		}
+		
+		return roles.toArray(new String[roles.size()]);
+		
 	}
 
 
