c54a25df0aea9ab0a1687f7395c41002e39b03ff
Mark Determan <mdeterman@gmail.com>
2012-05-22 13:02:31 -0500

    add copyright text clean up code

--- src/main/java/com/khs/sherpa/json/service/SpringAuthentication.java
+++ src/main/java/com/khs/sherpa/json/service/SpringAuthentication.java
@@ -1,5 +1,21 @@
 package com.khs.sherpa.json.service;
 
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,15 +29,8 @@ import com.khs.sherpa.exception.SherpaPermissionExcpetion;
 
 public class SpringAuthentication implements UserService {
 
-//	private ApplicationContext ctx;
-	
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
-//	public SpringAuthentication(ApplicationContext ctx) {
-//		this.ctx = ctx;
-//		authenticationManager = this.ctx.getBean(AuthenticationManager.class);
-//	}
 	
 	public String[] authenticate(String username, String password) {
 		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
--- src/main/java/com/khs/sherpa/servlet/SpringSherpaServlet.java
+++ src/main/java/com/khs/sherpa/servlet/SpringSherpaServlet.java
@@ -1,5 +1,21 @@
 package com.khs.sherpa.servlet;
 
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -15,7 +31,6 @@ import com.khs.sherpa.util.SpringSettingsLoader;
 
 public class SpringSherpaServlet extends FrameworkServlet {
 
-//	private static Logger LOG = Logger.getLogger(SpringSherpaServlet.class.getName());
 	
 	private static final long serialVersionUID = -6712767422014510622L;
 
@@ -32,8 +47,6 @@ public class SpringSherpaServlet extends FrameworkServlet {
 		sherpa.setSessionStatus(sessionStatus);
 		sherpa.loadRequest(request, response);
 	
-//		sherpa.setRoles("ROLE_USER");
-		
 		sherpa.setTarget(ReflectionCache.getObject(sherpa.getEndpoint()));
 		sherpa.run();
 		
