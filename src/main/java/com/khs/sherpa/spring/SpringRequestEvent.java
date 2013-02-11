a9f2383676d5a48c4f33decd2da14ec8545a2be2
Mark Determan <mdeterman@gmail.com>
2013-02-11 09:26:04 -0600

    add the request into the RequestContext.. So you can fetch the request using the Reqeust Context

--- src/main/java/com/khs/sherpa/spring/SpringRequestEvent.java
+++ src/main/java/com/khs/sherpa/spring/SpringRequestEvent.java
@@ -5,8 +5,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.khs.sherpa.context.ApplicationContext;
 import com.khs.sherpa.events.RequestEvent;
@@ -15,19 +19,18 @@ public class SpringRequestEvent implements RequestEvent {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringRequestEvent.class);
 	
+	private static final String REQUEST_ATTRIBUTES_ATTRIBUTE = RequestContextListener.class.getName() + ".REQUEST_ATTRIBUTES";
+	
 	public void before(ApplicationContext applicationContext, HttpServletRequest request, HttpServletResponse response) {
-		if(LOGGER.isTraceEnabled()) {
-			LOGGER.debug("Before request... ");
-			LOGGER.debug("Session: " + request.getSession().getId());
-			LOGGER.debug("Session timeout: " + request.getSession().getMaxInactiveInterval());
-		}
 		SecurityContext context = (SecurityContext) request.getSession().getAttribute("SPRING_SECURITY_CONTEXT_KEY");
 		if(context != null) {
-			if(LOGGER.isDebugEnabled()) {
-				LOGGER.debug("Loading... " + context);
-			}
 			SecurityContextHolder.setContext(context);
 		}
+		
+		ServletRequestAttributes attributes = new ServletRequestAttributes(request);
+		request.setAttribute(REQUEST_ATTRIBUTES_ATTRIBUTE, attributes);
+		LocaleContextHolder.setLocale(request.getLocale());
+		RequestContextHolder.setRequestAttributes(attributes);
 		
 	}
 
